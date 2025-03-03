223c0c307b001cc87375b274de52ced8816af500
Luiz Parente <lp.parente@gmail.com>
2025-03-03 04:32:56 -0500

    running experiment for 20 runs of SlowLogisticRegression.

--- src/utilities/Stopwatch.py
+++ src/utilities/Stopwatch.py
@@ -6,17 +6,17 @@ class Stopwatch:
 
     def start(self):
         """Starts the stopwatch."""
-        if self._start_time is not None:
-            raise Exception("Stopwatch is running. Use .stop() to stop it.")
-        
         self._start_time = time.perf_counter()
 
     def stop(self):
         """Stops the stopwatch and returns the elapsed time in seconds."""
-        if self._start_time is None:
-            raise Exception("Stopwatch is not running. Use .start() to start it.")
-        
         elapsed_time = time.perf_counter() - self._start_time
         self._start_time = None
+        
+        return elapsed_time
+
+    def pause(self):
+        """Pauses the stopwatch."""
+        elapsed_time = time.perf_counter() - self._start_time
         
         return elapsed_time
